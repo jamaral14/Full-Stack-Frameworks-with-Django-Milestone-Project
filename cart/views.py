b14721b558e6c48600e5c5b886ddeeb8257e239e
jamaral14 <amaralmm14@hotmail.com>
2020-02-10 19:59:48 +0000

    added method add_to_cart

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
-def view_cart(request):
+def view_cart(request): # View that renders the cart contents page
     return render(request, "cart.html")
+
+def add_to_cart(request, id): # Add a quantity of wine product to the cart
+    quantity = int(request.POST.get('quantity'))
+
+    cart = request.session.get('cart', {})
+    cart[id] = cart.get(id, quantity)
+
+    request.session['cart'] = cart
+    return redirect(reverse('index')) 
+
