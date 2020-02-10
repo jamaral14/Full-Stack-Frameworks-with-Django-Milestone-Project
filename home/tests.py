6b074af367f509f6a3fccc728de85b5c7491701b
jamaral14 <amaralmm14@hotmail.com>
2020-02-10 18:44:51 +0000

    removing text

--- home/tests.py
+++ home/tests.py
@@ -4,9 +4,6 @@ from .models import Product
 # Create your tests here.
 class ProductTests(TestCase):
 
-    """
-    Here we'll define the tests 
-    """
     def test_str(self):
         test_name = Product(name='A product')
         self.assertEqual(str(test_name), 'A product')
