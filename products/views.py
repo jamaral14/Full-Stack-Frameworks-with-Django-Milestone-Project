6501c95022fabcdbdb7906ce2aeb8515e7034e52
jamaral14 <amaralmm14@hotmail.com>
2020-02-10 12:11:26 +0000

    added 'def all_products' that Will return all the products that are in the database

--- products/views.py
+++ products/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 from .models import Product
 
 # Create your views here.
+def all_products(request):
+    """ Will return all the products that are in the database """
+    products = Product.objects.all()
+    return render(request, "products.html", {"products": products})
