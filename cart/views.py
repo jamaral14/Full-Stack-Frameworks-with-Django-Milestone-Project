30ef205e1aac0b4f6ab696e93722faee71467dc5
jamaral14 <amaralmm14@hotmail.com>
2020-02-10 20:04:31 +0000

    added method adjust_cart to allowed to increase and decrease the quantity

--- cart/views.py
+++ cart/views.py
@@ -13,3 +13,16 @@ def add_to_cart(request, id): # Add a quantity of wine product to the cart
     request.session['cart'] = cart
     return redirect(reverse('index')) 
 
+def adjust_cart(request, id): # Adjust the quantity of the product
+
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[id] = quantity
+    else:
+        cart.pop(id)
+    
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))    
+
