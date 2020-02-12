3732a4c42e68e0cb963752ed2c846d7fa1591d10
jamaral14 <amaralmm14@hotmail.com>
2020-02-12 20:17:38 +0000

    added class OrderLineItem method

--- checkout/models.py
+++ checkout/models.py
@@ -3,6 +3,7 @@ from products.models import Product
 
 # Create your models here.
 
+
 class Order(models.Model):
     full_name = models.CharField(max_length=50, blank=False)
     phone_number = models.CharField(max_length=20, blank=False)
@@ -16,3 +17,13 @@ class Order(models.Model):
 
     def __str__(self):
         return "{0}-{1}-{2}".format(self.id, self.date, self.full_name)
+
+
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(Order, null=False)
+    product = models.ForeignKey(Product, null=False)
+    quantity = models.IntegerField(blank=False)
+
+    def __str__(self):
+        return "{0} {1} @ {2}".format(
+            self.quantity, self.product.name, self.product.price)
