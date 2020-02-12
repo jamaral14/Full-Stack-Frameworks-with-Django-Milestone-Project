6e2a143eb6cfb1321ca21d597dcabb125002be0b
jamaral14 <amaralmm14@hotmail.com>
2020-02-12 20:44:46 +0000

    added a def checkout request

--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .forms import MakePaymentForm, OrderForm
+from django.conf import settings
+import stripe
+
 
 # Create your views here.
 
 stripe.api_key = settings.STRIPE_SECRET
 
 @login_required()
+def checkout(request):
+    if request.method == "POST":
+        order_form = OrderForm(request.POST)
+        payment_form = MakePaymentForm(request.POST)
