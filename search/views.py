ca349a510081fcea7c4847fda4516af854a96854
jamaral14 <amaralmm14@hotmail.com>
2020-02-12 11:28:35 +0000

    Changed 'q' to 'search' to better understand

--- search/views.py
+++ search/views.py
@@ -3,5 +3,5 @@ from products.models import Product
 
 # Create your views here.
 def do_search(request):
-    products = Product.objects.filter(name__icontains=request.GET['q'])
+    products = Product.objects.filter(name__icontains=request.GET['search'])
     return render(request, "products.html", {"products": products})
