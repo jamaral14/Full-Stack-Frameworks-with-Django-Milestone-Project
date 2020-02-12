cfd264a51f7b3f017d004106a7ba8cf85de70ff0
jamaral14 <amaralmm14@hotmail.com>
2020-02-12 20:31:06 +0000

    added MakePaymentForm for Payment details

--- checkout/forms.py
+++ checkout/forms.py
@@ -1,2 +1,15 @@
 from django import forms
 from .models import Order
+
+# Payment details - Credit card, ccv, experiry month and year, and stripeID
+
+class MakePaymentForm(forms.Form):
+
+    MONTH_CHOICES = [(i, i) for i in range(1, 12)]
+    YEAR_CHOICES = [(i, i) for i in range(2019, 2036)]
+
+    credit_card_number = forms.CharField(label='Credit card number', required=False)
+    cvv = forms.CharField(label='Security code (CVV)', required=False)
+    expiry_month = forms.ChoiceField(label='Month', choices=MONTH_CHOICES, required=False)
+    expiry_year = forms.ChoiceField(label='Year', choices=YEAR_CHOICES, required=False)
+    stripe_id = forms.CharField(widget=forms.HiddenInput)
