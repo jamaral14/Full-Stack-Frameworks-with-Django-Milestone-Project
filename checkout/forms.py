6cb3e2fc5e8468a9b88c2041e91d599458065ebd
jamaral14 <amaralmm14@hotmail.com>
2020-02-12 20:34:54 +0000

    class OrderForm(forms.ModelForm)

--- checkout/forms.py
+++ checkout/forms.py
@@ -3,13 +3,29 @@ from .models import Order
 
 # Payment details - Credit card, ccv, experiry month and year, and stripeID
 
+
 class MakePaymentForm(forms.Form):
 
     MONTH_CHOICES = [(i, i) for i in range(1, 12)]
     YEAR_CHOICES = [(i, i) for i in range(2019, 2036)]
 
-    credit_card_number = forms.CharField(label='Credit card number', required=False)
+    credit_card_number = forms.CharField(
+    label='Credit card number', required=False)
     cvv = forms.CharField(label='Security code (CVV)', required=False)
-    expiry_month = forms.ChoiceField(label='Month', choices=MONTH_CHOICES, required=False)
-    expiry_year = forms.ChoiceField(label='Year', choices=YEAR_CHOICES, required=False)
+    expiry_month = forms.ChoiceField(
+    label='Month', choices=MONTH_CHOICES, required=False)
+    expiry_year = forms.ChoiceField(
+    label='Year', choices=YEAR_CHOICES, required=False)
     stripe_id = forms.CharField(widget=forms.HiddenInput)
+
+  # More Details and prove of address, full name, country
+
+class OrderForm(forms.ModelForm):
+
+    class Meta:
+        model = Order
+        fields = (
+            'full_name', 'phone_number', 'country', 'postcode',
+            'town_or_city', 'street_address1', 'street_address2',
+            'county'
+        )
