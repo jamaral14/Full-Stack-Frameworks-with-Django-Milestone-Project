0dba206e305de52c12273a515d07547f1d036e6b
jamaral14 <amaralmm14@hotmail.com>
2020-02-12 20:47:04 +0000

    added a order save and order time

--- checkout/views.py
+++ checkout/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .forms import MakePaymentForm, OrderForm
 from django.conf import settings
+from django.utils import timezone
 import stripe
 
 
@@ -14,3 +15,8 @@ def checkout(request):
     if request.method == "POST":
         order_form = OrderForm(request.POST)
         payment_form = MakePaymentForm(request.POST)
+
+         if order_form.is_valid() and payment_form.is_valid():
+            order = order_form.save(commit=False)
+            order.date = timezone.now()
+            order.save()
