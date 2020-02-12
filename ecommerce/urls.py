3adf425c4e0b7dae41f7698ad5b8567340b7e8cb
jamaral14 <amaralmm14@hotmail.com>
2020-02-12 10:25:00 +0000

    Added urlspatterns  url(r'^search/', include(urls_search)) to ecommerce/urls.py

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -29,5 +29,6 @@ urlpatterns = [
     url(r'^accounts/', include(urls_accounts)),
     url(r'^products/', include(urls_products)),
     url(r'^cart/', include(urls_cart)),
+    url(r'^search/', include(urls_search)),
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT})
 ]
