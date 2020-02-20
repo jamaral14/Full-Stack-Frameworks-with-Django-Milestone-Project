6f084bd5ca1d40db4ea9fde67bc393300a270633
jamaral14 <amaralmm14@hotmail.com>
2020-02-20 10:56:58 +0000

    Added Stripe.createToken this function will prenvent the credit card details from being submitted to our server

--- static/js/stripe.js
+++ static/js/stripe.js
@@ -8,5 +8,22 @@ $(function() {
             cvc: $("#id_cvv").val()
         };
 
+    Stripe.createToken(card, function(status, response) {
+        if (status === 200) {
+            $("#credit-card-errors").hide();
+            $("#id_stripe_id").val(response.id);
+
+            // Prevent the credit card details from being submitted
+            // to our server
+            $("#id_credit_card_number").removeAttr('name');
+            $("#id_cvv").removeAttr('name');
+            $("#id_expiry_month").removeAttr('name');
+            $("#id_expiry_year").removeAttr('name');
+
+            form.submit();
+
+      }    
+
+    })   
     })
 })        
