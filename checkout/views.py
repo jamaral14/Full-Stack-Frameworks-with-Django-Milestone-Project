2688d431f1eb128956fa64a586adea3989e9896c
jamaral14 <amaralmm14@hotmail.com>
2020-02-20 10:07:43 +0000

    Added imports Product, OrderLineItem, messages and get_object_or_404, reverse, redirect. Also finished if statment for payments with card and messages

--- checkout/views.py
+++ checkout/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import MakePaymentForm, OrderForm
+from django.contrib import messages
+from .models import OrderLineItem
 from django.conf import settings
 from django.utils import timezone
+from products.models import Product
 import stripe
 
 
@@ -20,3 +23,28 @@ def checkout(request):
             order = order_form.save(commit=False)
             order.date = timezone.now()
             order.save()
+
+             try:
+                customer = stripe.Charge.create(
+                    amount=int(total * 100),
+                    currency="EUR",
+                    description=request.user.email,
+                    card=payment_form.cleaned_data['stripe_id']
+                )
+            except stripe.error.CardError:
+                messages.error(request, "Your card was declined!")
+            
+            if customer.paid:
+                messages.error(request, "You have successfully paid")
+                request.session['cart'] = {}
+                return redirect(reverse('products'))
+            else:
+                messages.error(request, "Unable to take payment")
+        else:
+            print(payment_form.errors)
+            messages.error(request, "We were unable to take a payment with that card!")
+    else:
+        payment_form = MakePaymentForm()
+        order_form = OrderForm()
+    
+    return render(request, "checkout.html", {"order_form": order_form, "payment_form": payment_form, "publishable": settings.STRIPE_PUBLISHABLE})
