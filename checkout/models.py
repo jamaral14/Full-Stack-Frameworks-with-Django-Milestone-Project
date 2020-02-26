f5e1e74a5ecec2d3ededfbbed089783ab2a221b9
jamaral14 <amaralmm14@hotmail.com>
2020-02-26 15:49:44 +0000

    imported CountryField from django_countries.fields

--- checkout/models.py
+++ checkout/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from products.models import Product
+from django_countries.fields import CountryField
 
 # Create your models here.
 
@@ -7,7 +8,8 @@ from products.models import Product
 class Order(models.Model):
     full_name = models.CharField(max_length=50, blank=False)
     phone_number = models.CharField(max_length=20, blank=False)
-    country = models.CharField(max_length=40, blank=False)
+    name = models.CharField(max_length=100, blank=False)
+    country = CountryField()
     postcode = models.CharField(max_length=20, blank=True)
     town_or_city = models.CharField(max_length=40, blank=False)
     street_address1 = models.CharField(max_length=40, blank=False)
