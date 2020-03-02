9a91f5974750eece6f3ba5f98e3649d1f6a3fe59
jamaral14 <amaralmm14@hotmail.com>
2020-03-02 22:50:09 +0000

    Added space on DATABASE

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 if "DATABASE_URL" in os.environ:
-DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
 else:
     print("Database URL not found. Using SQLite instead")
     DATABASES = {
