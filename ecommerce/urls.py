c67ee39b0e8a9f9f3dea9cde64151793b6cbaaea
jamaral14 <amaralmm14@hotmail.com>
2020-03-02 15:47:36 +0000

    no changes

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -32,5 +32,5 @@ urlpatterns = [
     url(r'^cart/', include(urls_cart)),
     url(r'^checkout/', include(urls_checkout)),
     url(r'^search/', include(urls_search)),
-    url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT})
+    url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT}),
 ]
