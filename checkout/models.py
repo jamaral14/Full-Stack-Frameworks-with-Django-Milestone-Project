1382d671d332d8ff8dbf3f44120f01606b3097e2
jamaral14 <amaralmm14@hotmail.com>
2020-03-02 20:11:49 +0000

    removed name = models.CharField(max_length=100, blank=False)

--- checkout/models.py
+++ checkout/models.py
@@ -8,7 +8,6 @@ from django_countries.fields import CountryField
 class Order(models.Model):
     full_name = models.CharField(max_length=50, blank=False)
     phone_number = models.CharField(max_length=20, blank=False)
-    name = models.CharField(max_length=100, blank=False)
     country = CountryField()
     postcode = models.CharField(max_length=20, blank=True)
     town_or_city = models.CharField(max_length=40, blank=False)
